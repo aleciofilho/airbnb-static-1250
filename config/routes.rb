baae45806f4b9ab92c397b1bce3b71cd6ce23ad8
Alecio Filho <alecio_filho@hotmail.com>
2023-05-23 08:53:19 -0300

    index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "flats#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/flats/:id", to: "flats#show", as: :flat 
 end
+
